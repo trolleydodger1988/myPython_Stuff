981157fbbb8af179154f0c2be72d2821c10a8c78
trolleydodger1988 <116392514+trolleydodger1988@users.noreply.github.com>
2024-01-12 09:42:13 -0800

    Delete Mathler/not_in_list_test.py

--- Mathler/not_in_list_test.py
+++
@@ -1,9 +0,0 @@
-my_list = [("1","2","3", "+"), ("1","4","5", "-"), ("1","2","6", "/")]
-new_list =[]
-char = ["1","2", "/"]
-for i in range(len(my_list)):
-    for j in range(len(char)):
-        if char[j] not in my_list[i]:
-            new_list.append(my_list[i])
-    
-print(new_list)
